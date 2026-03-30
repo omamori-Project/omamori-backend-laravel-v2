98cd2c0e0bc927b3727821393d831b97feaa3419
Azuki.I <azuking69@hotmail.co.jp>
2026-03-30 22:02:12 +0900

    26.03.30 made BaseRepository.php

---
+++ app/Common/Base/BaseRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Common\Base;
+
+// import
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository
+{
+    protected Model $model;
+
+    // 새로운 것을 작성
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+
+
+    // id 찾기
+    public function findById(int $id): ?Model
+    {
+        return $this->model->find($id);
+    }
+
+
+    // 삭제
+    public function delete(int $id): bool
+    {
+        $item = $this->findById($id);
+        if(!$item){
+            return false;
+        }
+        return $item->delete();
+    }
+}
