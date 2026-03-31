c2d7c6abcacc2f6322d621e365dd9fd621aea5a0
Azuki.I <azuking69@hotmail.co.jp>
2026-03-31 20:39:40 +0900

    26.03.31 inserted show of api.php

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,6 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::middleware('jwt.auth')->group(function(){
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
+
+// 내 정보 조회
+Route::get('/me', [AuthController::class, 'show']);
