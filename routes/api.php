1046b0ee002d5f7030c971584b0a37049864ec59
Azuki.I <azuking69@hotmail.co.jp>
2026-04-03 00:14:19 +0900

    feat: Google OAuth 로그인 기능 구현 (#7)
    * 26.04.02 inserted google OAuth of api.php
    
    * 26.04.02 inserted google OAuth of web.php
    
    * 26.04.02 inserted google OAuth of config/service.php
    
    * 26.04.02 made google OAuth of migrations
    
    * 26.04.02 inserted google OAuth of AuthController.php
    
    * 26.04.02 inserted google OAuth of User.php
    
    * 26.04.02 inserted google OAuth of AuthService.php
    
    * 26.04.02 changed codes of AuthController.php
    
    * 26.04.02 changed codes of AuthService.php
    
    * 26.04.02 changed codes of api.php
    
    * 26.04.02 changed codes of AuthController.php

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,8 @@ Route::middleware('jwt.auth')->group(function(){
     Route::delete('/me', [UserProfileController::class, 'destroy']);
 });
 
+// Google OAuth 시작
+Route::get('/auth/google', [AuthController::class, 'googleRedirect']);
+
+// Google OAuth 콜백
+Route::get('/auth/google/callback', [AuthController::class, 'googleCallback']);
